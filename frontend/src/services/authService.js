52a77aa784fb65abbc8c4511b73d3b8ec6312746
Aditya Kumar Karan <adityakaran973@gmail.com>
2026-09-24 01:52:57 +0530

    Fix React Router routes on Vercel

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://authorabackend.vercel.app/api/auth";
 
 export const registerUser = async (userData) => {
   const response = await axios.post(
